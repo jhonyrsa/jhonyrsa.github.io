c1b36b21c226710ed5d89d7f21e6cc0d06c7bec6
Jhony Almeida <jhonyrsa@gmail.com>
2026-04-20 23:41:25 -0300

    Deploy website - based on c3f7c019f0b50803821d0403c0d20874f999f203

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("C:\\Users\\jhonyrsa\\Desktop\\Cursos\\jhony-site\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\jhonyrsa\\Desktop\\Cursos\\jhony-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\jhonyrsa\\Desktop\\Cursos\\jhony-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\jhonyrsa\\Desktop\\Cursos\\jhony-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\jhonyrsa\\Desktop\\Cursos\\jhony-site\\src\\css\\custom.css"),
-];
